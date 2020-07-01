287af1cfd3042326f77bd64135020ad843a35c5f
cwc329 <test@test.com>
2020-07-01 20:36:15 +0800

    hw3 done

---
+++ homeworks/week4/hw3.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const process = require('process');
+
+request(
+  {
+    url: `https://restcountries.eu/rest/v2/name/${process.argv[2]}`,
+  },
+  (error, response, body) => {
+    const jsonNations = JSON.parse(body);
+    for (let i = 0; i < jsonNations.length; i += 1) {
+      console.log(`
+      ============
+      國家：${jsonNations[i].name}
+      首都：${jsonNations[i].capital}
+      貨幣：${jsonNations[i].currencies[0].code}
+      國碼：${jsonNations[i].callingCodes}
+      `);
+    }
+  },
+);
