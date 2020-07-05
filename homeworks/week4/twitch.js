35e0a523c2ab7fb47f7d0e621bb0adef989da1d4
cwc329 <test@test.com>
2020-07-05 20:15:49 +0800

    twitch.js revised

--- homeworks/week4/twitch.js
+++ homeworks/week4/twitch.js
@@ -4,31 +4,45 @@ const process = require('process');
 
 const gameName = process.argv[2];
 const result = [];
+const startPoint = 0;
 
-function searchStreamByGame(arr, name) {
-  for (let i = 0; i <= 100; i += 100) {
-    request.get(
-      {
-        url: `https://api.twitch.tv/kraken/streams?limit=100&offset=${i}&game=${name}`,
-        headers: {
-          'Client-ID': 'l8v669ong3mcmzuafjnny8y0tcj9vp',
-          Accept: 'application/vnd.twitchtv.v5+json',
-        },
-      },
-      (error, response, body) => {
-        if (error) {
-          console.log(error);
-          return;
-        }
-        const jsonChannelData = JSON.parse(body).streams;
-        for (let j = 0; j < jsonChannelData.length; j += 1) {
-          const channelName = jsonChannelData[j].channel.display_name;
-          const channelId = jsonChannelData[j].channel._id;
-          console.log(channelName, channelId);
-          arr.push([channelName, channelId]);
-        }
-      },
-    );
+function printStreams(arr) {
+  console.log('result: ', arr.length);
+  if (arr.length === 0) {
+    console.log('not found.');
+  } else {
+    arr.forEach((ele) => {
+      console.log(ele[1], ele[0]);
+    });
   }
 }
-searchStreamByGame(result, gameName);
+
+function searchStreamByGame(arr, name, offset, callback) {
+  request.get(
+    {
+      url: `https://api.twitch.tv/kraken/streams?limit=100&offset=${offset * 100}&game=${name}`,
+      headers: {
+        'Client-ID': 'l8v669ong3mcmzuafjnny8y0tcj9vp',
+        Accept: 'application/vnd.twitchtv.v5+json',
+      },
+    },
+    (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      const jsonChannelData = JSON.parse(body).streams;
+      for (let j = 0; j < jsonChannelData.length; j += 1) {
+        const channelName = jsonChannelData[j].channel.display_name;
+        const channelId = jsonChannelData[j].channel._id;
+        arr.push([channelName, channelId]);
+      }
+      if (offset < 1) {
+        searchStreamByGame(arr, name, offset + 1, callback);
+      } else {
+        callback(arr);
+      }
+    },
+  );
+}
+searchStreamByGame(result, gameName, startPoint, printStreams);
