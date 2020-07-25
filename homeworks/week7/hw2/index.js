30f0574d17e437e595795aaa1136e1717d16e1fd
cwc329 <test@test.com>
2020-07-26 01:13:53 +0800

    hw4 done

--- homeworks/week7/hw2/index.js
+++ homeworks/week7/hw2/index.js
@@ -7,17 +7,11 @@ for (let i = 0; i < answers.length; i += 1) {
 
 for (let i = 0; i < QuestionDiv.length; i += 1) {
   QuestionDiv[i].addEventListener('click', (e) => {
-    let answer;
-    if (e.target.tagName === 'DIV') {
-      answer = e.target.nextElementSibling;
-    } else if (e.target.tagName === 'SPAN') {
-      answer = e.target.parentElement.nextElementSibling;
-    }
+    const answer = e.target.closest('div').nextElementSibling;
     if (answer.style.display === 'none') {
       answer.style.display = 'block';
     } else {
       answer.style.display = 'none';
     }
-    console.log(e.target);
   });
 }
