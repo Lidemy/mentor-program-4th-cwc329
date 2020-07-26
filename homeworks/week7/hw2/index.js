fb960deeca9cf9948af97d192e92a1e376b32826
cwc329 <test@test.com>
2020-07-26 19:34:15 +0800

    hw2 revised again

--- homeworks/week7/hw2/index.js
+++ homeworks/week7/hw2/index.js
@@ -1,8 +1,6 @@
-const QuestionDiv = document.querySelectorAll('.FAQ__content--question');
+const FAQ = document.querySelector('.FAQ');
 
-for (let i = 0; i < QuestionDiv.length; i += 1) {
-  QuestionDiv[i].addEventListener('click', (e) => {
-    const answer = e.target.closest('div').nextElementSibling;
-    answer.classList.toggle('active');
-  });
-}
+FAQ.addEventListener('click', (e) => {
+  const answer = e.target.closest('.FAQ__content--question').nextElementSibling;
+  answer.classList.toggle('active');
+});
