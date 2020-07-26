393acf039e5df601e021f3715df587d9337c5301
cwc329 <test@test.com>
2020-07-26 19:28:18 +0800

    hw2 revised

--- homeworks/week7/hw2/index.js
+++ homeworks/week7/hw2/index.js
@@ -1,17 +1,8 @@
-const answers = document.querySelectorAll('.FAQ__content--answer');
 const QuestionDiv = document.querySelectorAll('.FAQ__content--question');
-
-for (let i = 0; i < answers.length; i += 1) {
-  answers[i].style.display = 'none';
-}
 
 for (let i = 0; i < QuestionDiv.length; i += 1) {
   QuestionDiv[i].addEventListener('click', (e) => {
     const answer = e.target.closest('div').nextElementSibling;
-    if (answer.style.display === 'none') {
-      answer.style.display = 'block';
-    } else {
-      answer.style.display = 'none';
-    }
+    answer.classList.toggle('active');
   });
 }
