45fa934580964f4be502d3c52a8aa669af62b906
cwc329 <test@test.com>
2020-07-29 01:28:50 +0800

    week7 challenge added animation

--- homeworks/week7/challenge/index.js
+++ homeworks/week7/challenge/index.js
@@ -2,25 +2,33 @@ const fwBtn = document.querySelector('.controls__fwBtn');
 const bwBtn = document.querySelector('.controls__bwBtn');
 const sliders = document.querySelector('.sliders').children;
 let currentSliderIndex = 0;
-sliders[currentSliderIndex].classList.toggle('active');
+
+function changeImg(currentInd, direction) {
+  let newInd;
+  if (direction === 'forward') {
+    newInd = currentInd + 1;
+    if (!sliders[newInd]) {
+      newInd = 0;
+    }
+    sliders[currentInd].classList.add('previous');
+  } else {
+    newInd = currentInd - 1;
+    if (!sliders[newInd]) {
+      newInd = sliders.length - 1;
+    }
+    sliders[currentInd].classList.add('next');
+  }
+  sliders[currentInd].classList.remove('show');
+  sliders[newInd].classList.add('show');
+  sliders[newInd].classList.remove('next');
+  sliders[newInd].classList.remove('previous');
+  return newInd;
+}
 
 fwBtn.addEventListener('click', () => {
-  sliders[currentSliderIndex].classList.toggle('active');
-  let nextSliderIndex = currentSliderIndex + 1;
-  console.log(sliders);
-  if (!sliders[nextSliderIndex]) {
-    nextSliderIndex = 0;
-  }
-  sliders[nextSliderIndex].classList.toggle('active');
-  currentSliderIndex = nextSliderIndex;
+  currentSliderIndex = changeImg(currentSliderIndex, 'forward');
 });
 
 bwBtn.addEventListener('click', () => {
-  sliders[currentSliderIndex].classList.toggle('active');
-  let previousSliderIndex = currentSliderIndex - 1;
-  if (!sliders[previousSliderIndex]) {
-    previousSliderIndex = sliders.length - 1;
-  }
-  sliders[previousSliderIndex].classList.toggle('active');
-  currentSliderIndex = previousSliderIndex;
+  currentSliderIndex = changeImg(currentSliderIndex, 'left');
 });
