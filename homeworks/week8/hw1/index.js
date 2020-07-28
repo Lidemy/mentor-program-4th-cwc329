4202649ed7a682cbae761e5c45321e194c2a3625
cwc329 <test@test.com>
2020-07-28 23:35:01 +0800

    hw1 revised

--- homeworks/week8/hw1/index.js
+++ homeworks/week8/hw1/index.js
@@ -4,33 +4,44 @@ const request = new XMLHttpRequest();
 
 const prizes = document.querySelectorAll('.lottery__prize');
 let currentPrizeIndex = 0;
+
+function changeImg(currentInd, newInd) {
+  prizes[currentInd].classList.remove('show');
+  prizes[currentInd].classList.add('hide');
+  prizes[newInd].classList.add('show');
+  prizes[newInd].classList.remove('hide');
+  return newInd;
+}
+
 document.querySelector('.lottery').addEventListener('click', (e) => {
-  if (e.target.classList.contains('lottery__btn')) {
+  if (e.target.classList.contains('pick')) {
     request.onload = () => {
       try {
         JSON.parse(request.response);
       } catch (err) {
+        currentPrizeIndex = changeImg(currentPrizeIndex, 0);
         alert('系統不穩定，請再試一次');
         return;
       }
       let { prize } = JSON.parse(request.response);
       if (!prize) {
+        currentPrizeIndex = changeImg(currentPrizeIndex, 0);
         alert('系統不穩定，請再試一次');
         return;
       }
-      prizes[currentPrizeIndex].classList.remove('show');
-      prizes[currentPrizeIndex].classList.add('hide');
       prize = prize.toLowerCase();
+      console.log(prize);
       for (let i = 0; i < prizes.length; i += 1) {
         if (prizes[i].classList.contains(prize)) {
-          prizes[i].classList.add('show');
-          prizes[i].classList.remove('hide');
-          currentPrizeIndex = i;
+          currentPrizeIndex = changeImg(currentPrizeIndex, i);
           break;
         }
       }
     };
     request.open('GET', ApiEndpoint, true);
     request.send();
+  }
+  if (e.target.classList.contains('back')) {
+    currentPrizeIndex = changeImg(currentPrizeIndex, 0);
   }
 });
