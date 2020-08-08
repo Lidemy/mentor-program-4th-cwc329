29430c86c0a6f98783b356aae69fdbc8214c72df
cwc329 <test@test.com>
2020-08-08 21:28:16 +0800

    added html escape

--- homeworks/week9/hw1/add_comment.php
+++ homeworks/week9/hw1/add_comment.php
@@ -4,12 +4,16 @@
   require_once('utils.php');
   $id = $_SESSION['id'];
   $nickname = getUserData($id)['nickname'];
-  $comment = htmlspecialchars($_POST['comment']);
+  echo $_POST['comment'] . '<br>';
+  $comment = htmlspecialchars($_POST['comment'],ENT_QUOTES);
+  echo $comment;
   $sql = sprintf("INSERT INTO cwc329_comments (nickname, comment) VALUE ('%s', '%s')", $nickname, $comment);
+  
   $conn->query($sql);
   if (!empty($conn->error)) {
     echo $conn->error;
     die();
   } 
   header('Location: index.php');
+  
 ?>
